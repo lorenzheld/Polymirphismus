2a4ab254187903c075453050737f5e3bf970a1df
Lorenz Held <held.lorenz.08@gmail.com>
2024-11-27 09:55:22 +0100

    change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,7 @@ public class Main {
         Vogel meinHuhn = new Huhn();
         meinHuhn.fliegen(); // Ausgabe: "Ein Huhn kann nur ein bisschen flattern."
 
-        // Spezifische Methoden testen (falls möglich)
-        // meineEnte.schwimmen(); // Fehler: Methode nicht in Vogel definiert
-        // meinHuhn.gackern();    // Fehler: Methode nicht in Vogel definiert
 
-        // Direkter Zugriff auf Unterklassen-Methoden
         Ente echteEnte = new Ente();
         echteEnte.schwimmen(); // Ausgabe: "Eine Ente schwimmt."
 
